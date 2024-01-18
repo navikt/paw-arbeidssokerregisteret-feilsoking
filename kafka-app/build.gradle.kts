0c8db9cea42ce58f499f1f7d5f269cc1cd160989
Nils Martin Sande <nils.martin.sande@nav.no>
2024-01-18 12:21:33 +0100

    La til gradle oppsett

---
+++ kafka-app/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    kotlin("jvm") version "1.9.20"
+    application
+}
+
+dependencies {
+}
+
+application {
+    mainClass.set("no.nav.paw.arbeidssokerregisteret.debug.app.AppKt")
+}
---
+++ kafka-app/src/main/kotlin/no/nav/paw/arbeidssokerregisteret/debug/app/App.kt
@@ -0,0 +1,5 @@
+package no.nav.paw.arbeidssokerregisteret.debug.app
+
+fun main() {
+
+}
