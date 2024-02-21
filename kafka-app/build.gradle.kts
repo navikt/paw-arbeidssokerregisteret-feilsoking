204f25b629575ecf78c5855f7d2bf7113d089f00
Nils Martin Sande <nils.martin.sande@nav.no>
2024-02-21 13:05:43 +0100

    Initiell test versjon av henting av record metadata basert på traceId

--- kafka-app/build.gradle.kts
+++ kafka-app/build.gradle.kts
@@ -1,14 +1,25 @@
 plugins {
     kotlin("jvm") version "1.9.20"
+    id("com.google.cloud.tools.jib") version "3.4.0"
     application
 }
+val jvmVersion = JavaVersion.VERSION_21
+val image: String? by project
 
-val pawUtilsVersion = "24.01.11.9-1"
+val pawUtilsVersion = "24.02.06.10-1"
 val hopliteVersion = "2.8.0.RC3"
 
+val navCommonModulesVersion = "3.2023.10.23_12.41-bafec3836d28"
+val tokenSupportVersion = "3.1.5"
+
 dependencies {
+    implementation("com.sksamuel.hoplite:hoplite-core:$hopliteVersion")
     implementation("no.nav.paw.kafka:kafka:$pawUtilsVersion")
     implementation("no.nav.paw.hoplite-config:hoplite-config:$pawUtilsVersion")
+    implementation("org.apache.kafka:kafka-clients:3.6.1")
+    implementation("io.confluent:kafka-avro-serializer:7.6.0")
+    implementation(pawObservability.bundles.ktorNettyOpentelemetryMicrometerPrometheus)
+
 }
 
 application {
@@ -17,6 +28,31 @@ application {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+        languageVersion = JavaLanguageVersion.of(jvmVersion.majorVersion)
     }
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://packages.confluent.io/maven/")
+    }
+    mavenNav("paw-arbeidssokerregisteret")
+}
+
+fun RepositoryHandler.mavenNav(repo: String): MavenArtifactRepository {
+    val githubPassword: String by project
+
+    return maven {
+        setUrl("https://maven.pkg.github.com/navikt/$repo")
+        credentials {
+            username = "x-access-token"
+            password = githubPassword
+        }
+    }
+}
+
+jib {
+    from.image = "ghcr.io/navikt/baseimages/temurin:${jvmVersion.majorVersion}"
+    to.image = "${image ?: rootProject.name }:${project.version}"
 }
--- kafka-app/src/main/kotlin/no/nav/paw/arbeidssokerregisteret/debug/app/App.kt
+++ kafka-app/src/main/kotlin/no/nav/paw/arbeidssokerregisteret/debug/app/App.kt
@@ -1,9 +1,88 @@
 package no.nav.paw.arbeidssokerregisteret.debug.app
 
+import io.confluent.kafka.serializers.KafkaAvroDeserializer
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import no.nav.paw.config.hoplite.loadNaisOrLocalConfiguration
-import no.nav.paw.config.kafka.KAFKA_CONFIG
 import no.nav.paw.config.kafka.KafkaConfig
+import no.nav.paw.config.kafka.KafkaFactory
+import org.apache.avro.generic.GenericRecord
+import org.apache.kafka.clients.consumer.Consumer
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.TopicPartition
+import org.apache.kafka.common.serialization.LongDeserializer
+import java.time.Duration
+import java.time.Instant
+import java.util.UUID
+
+const val OPPLYSNINGER_TOPIC = "paw.opplysninger-om-arbeidssoeker-beta-v7"
+const val PERIODE_TOPIC = "paw.arbeidssokerperioder-beta-v7"
+const val PROFILERING = "paw.arbeidssoker-profilering-beta-v2"
+
+val topics = listOf(OPPLYSNINGER_TOPIC, PERIODE_TOPIC, PROFILERING)
 
 fun main() {
-    val kafkaConfig = loadNaisOrLocalConfiguration<KafkaConfig>(KAFKA_CONFIG)
+    val kafkaConfig = loadNaisOrLocalConfiguration<KafkaConfig>("kafka")
+    val kafkaFactory = KafkaFactory(kafkaConfig)
+
+    fun consumer(): Consumer<Long, Any> = kafkaFactory.createConsumer(
+        groupId = "paw-arbeidssokerregisteret-debug-app-${UUID.randomUUID()}",
+        clientId = "paw-arbeidssokerregisteret-debug-app-${UUID.randomUUID()}",
+        keyDeserializer = LongDeserializer::class,
+        valueDeserializer = KafkaAvroDeserializer::class
+    )
+
+    embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/isAlive") {
+                call.respondText("I'm alive!")
+            }
+            get("/isReady") {
+                call.respondText("I'm ready!")
+            }
+            get("api-topics/by-trace-id/{traceId}") {
+                val traceId: String? = call.parameters["traceId"]
+                val partition: Int? = call.request.queryParameters["partition"]?.toIntOrNull()
+                if (traceId == null || partition == null) {
+                    call.respond(HttpStatusCode.BadRequest, "Missing params")
+                    return@get
+                } else {
+                    val topicPartitions = topics.map { topic -> TopicPartition(topic, partition) }
+                    val records = consumer().use { consumer ->
+                        consumer.assign(topicPartitions)
+                        consumer.seekToBeginning(topicPartitions)
+                        consumer.asSequence()
+                            .filter { record ->
+                                val traceparent = record.headers()
+                                    .lastHeader("traceparent")?.value()
+                                    ?.toString(Charsets.UTF_8)
+                                traceparent?.contains(traceId) == true
+                            }.map { record -> record.timestamp() to (record.value() as GenericRecord) }
+                            .map { (timestamp, generic) ->
+                                listOfNotNull(
+                                    "schema" to generic.schema.name,
+                                    "timestamp" to Instant.ofEpochMilli(timestamp)
+                                        .toString(),
+                                    "id" to generic.get("id")?.toString(),
+                                    "ref:periodeId" to generic.get("periodeId")
+                                        ?.toString(),
+                                    "ref:opplysningerOmArbeidssokerId" to generic.get("opplysningerOmArbeidssokerId")
+                                        ?.toString(),
+                                ).toMap()
+                            }.toList()
+                    }
+                    call.respond(records)
+                }
+            }
+        }
+    }.start(wait = true)
 }
+
+fun <K, V> Consumer<K, V>.asSequence(): Sequence<ConsumerRecord<K, V>> =
+    generateSequence {
+        this.poll(Duration.ofSeconds(2))
+    }.flatten()
